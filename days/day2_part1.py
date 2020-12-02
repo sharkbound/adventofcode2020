9766ca2e656f6fb2dd2f9dac09b339ff293ce891
sharkbound <laptopblaster@gmail.com>
2020-12-01 22:20:23 -0600

    prep for day 2

---
+++ days/day2_part1.py
@@ -0,0 +1,13 @@
+from day import Day
+from itertools import combinations
+
+
+class Day2Part1(Day):
+    day = 2
+    part = 1
+
+    def parse_input(self):
+        return self.input_text
+
+    def solve(self):
+        pass
---
+++ days/day2_part2.py
@@ -0,0 +1,13 @@
+from day import Day
+from itertools import combinations
+
+
+class Day2Part2(Day):
+    day = 2
+    part = 2
+
+    def parse_input(self):
+        return self.input_text
+
+    def solve(self):
+        pass
