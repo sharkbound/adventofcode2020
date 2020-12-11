d215cffd3c0600b9a8efdc186841eb2ddf3af917
sharkbound <laptopblaster@gmail.com>
2020-12-10 23:26:38 -0600

    day 10 done, had to reference other solutions for day 10 part 2, but i learned why it worked, so i am happy about the end result

--- days/day10_part2.py
+++ days/day10_part2.py
@@ -1,3 +1,6 @@
+from collections import defaultdict, deque, Counter
+from pprint import pprint
+
 from day import Day, util
 import numpy as np
 
@@ -10,13 +13,25 @@ class Day10Part2(Day):
         return '16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4'
 
     def parse_input(self):
-        # return np.fromstring(self.input_text, dtype=np.int64, sep='\n')
-        return [0] + (lst := list(map(int, self.get_sample_input().splitlines()))) + [max(lst) + 3]
+        return [0] + list(map(int, self.input_text.splitlines()))
+
+    def _check_can_connect(self, data, index, offset):
+        return index + offset < len(data) and data[index + offset] - data[index] <= 3
+
+    def _get_next_three_adapters(self, data, index):
+        return [data[index + i] for i in (1, 2, 3) if self._check_can_connect(data, index, i)]
 
     def solve(self):
         data = self.parse_input()
         data.sort()
 
-        print(*data, sep='\n')
-        valid = 0
-        print('day 10 part 2 answer:', valid)
+        # referenced from reddit comment: https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/gfcxuxf?utm_source=share&utm_medium=web2x&context=3
+        # this problem threw me in a very hard to escape mental block in understanding HOW to solve it
+        # while i didnt really write this myself, i did come up with (_get_next_three_adapters, and _check_can_connect) myself while struggling
+        # but this solution from the reddit comment finally made it click for me mentally HOW to solve it, and WHY this solved it
+        connection_counter = Counter({0: 1})
+        for i, current_node in enumerate(data):
+            for next_node in self._get_next_three_adapters(data, i):
+                connection_counter[next_node] += connection_counter[current_node]
+
+        print('day 10 part 2 answer:', connection_counter[data[-1]])
