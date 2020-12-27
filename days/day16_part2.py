bb995766689993dcf635b64f8b6bc0a9051d77bb
sharkbound <laptopblaster@gmail.com>
2020-12-26 22:30:57 -0600

    WIP day 16 part 2

--- days/day16_part2.py
+++ days/day16_part2.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 from dataclasses import dataclass
 
 from day import Day, util
@@ -9,10 +10,13 @@ RE_KEY_VALUE = re.compile(r'(.+): (\d+)-(\d+) or (\d+)-(\d+)')
 @dataclass()
 class Validator:
     key: str
-    index: int
     range1: range
     range2: range
     all_valid = set()
+    index: int = None
+
+    def has_index(self):
+        return self.index is not None
 
     def __post_init__(self):
         self.all_valid = {*self.range1, *self.range2}
@@ -23,36 +27,45 @@ class Day16Part2(Day):
     part = 2
 
     def get_sample_input(self):
-        return ('class: 1-3 or 5-7\n'
-                'row: 6-11 or 33-44\n'
-                'seat: 13-40 or 45-50\n\n'
+        return ('class: 0-1 or 4-19\n'
+                'row: 0-5 or 8-19\n'
+                'seat: 0-13 or 16-19\n\n'
                 'your ticket:\n'
-                '7,1,14\n\n'
+                '11,12,13\n\n'
                 'nearby tickets:\n'
-                '7,3,47\n'
-                '40,4,50\n'
-                '55,2,20\n'
-                '38,6,12')
+                '3,9,18\n'
+                '15,1,5\n'
+                '5,14,9')
 
-    def parse_validator_line(self, index: int, line: str):
+    def parse_validator_line(self, line: str):
         key, *nums = RE_KEY_VALUE.search(line).groups()
         r1_min, r1_max, r2_min, r2_max = map(int, nums)
-        return Validator(key, index, range(r1_min, r1_max + 1), range(r2_min, r2_max + 1))
+        return Validator(key, range(r1_min, r1_max + 1), range(r2_min, r2_max + 1))
+
+    def iter_column(self, tickets, column_index):
+        for ticket in tickets:
+            yield ticket[column_index]
 
     def parse_input(self):
         raw = self.input_text.split('\n\n')
-        validators = [self.parse_validator_line(i, line) for i, line in enumerate(raw[0].splitlines())]
-        my_ticket = [int(x) for x in re.findall('\d+', raw[1])]
+        validators = [self.parse_validator_line(line) for line in raw[0].splitlines()]
+        my_ticket = util.find_all_ints(raw[1])
         nearby_tickets = [util.find_all_ints(line) for line in raw[-1].splitlines()[1:]]
         return validators, my_ticket, nearby_tickets
 
-    def solve(self):
-        validators, my_ticket, tickets = self.parse_input()
-        invalid = [
-            number
-            for ticket in tickets
-            for number in ticket
-            if not any(number in v.all_valid for v in validators)
-        ]
+    def assign_validator_index(self, tickets: np.ndarray, validator: Validator):
+        for column in range(tickets.shape[1]):
+            if all(number in validator.all_valid for number in tickets[:, column]):
+                validator.index = column
+                return validator
+        return validator
 
-        print('16:2 =>', sum(invalid))
+    def solve(self):
+        validators, my_ticket, raw_tickets = self.parse_input()
+        valid_tickets = np.array([ticket for ticket in raw_tickets if any(number in v.all_valid for number in ticket for v in validators)])
+        # https://adventofcode.com/2020/day/16
+        for validator in validators:
+            print(self.assign_validator_index(valid_tickets, validator))
+
+        answer = sum(my_ticket[validator.index] for validator in validators if validator.key.startswith('departure'))
+        print('16:2 =>', answer)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from day import run_day
 
-run_day(16, 1, timed=False)
-# run_day(16, 2, timed=True)
-
+# run_day(16, 1, timed=False)
+run_day(16, 2, timed=False)
