f418b57b186f17fa5c7be5fdf79805f95cfa17f5
sharkbound <laptopblaster@gmail.com>
2021-08-29 14:09:38 -0500

    padding and z expansion is working

--- days/day17_part1.py
+++ days/day17_part1.py
@@ -1,28 +1,29 @@
 from collections import deque
+from copy import deepcopy
 from typing import Deque, Tuple, Generator
 
 from day import Day, util
 import re
 import numpy as np
 
-CubeType = Deque[dict]
-
 ACTIVE = '#'
 INACTIVE = '.'
+
+CubeType = Deque[np.ndarray]
 
 
 def get_neighbors(cubes: CubeType, x: int, y: int, z: int) -> Generator[str, None, None]:
     pass
 
 
-def expand_cubes(cubes: CubeType, center_z_index: int) -> Tuple[CubeType, int]:
-    if not cubes:
-        return deque(({}, {}, {})), 1
+def expand_cubes_z(cubes: CubeType):
+    cubes.appendleft(np.full(cubes[0].shape, '.', dtype='U1'))
+    cubes.append(np.full(cubes[0].shape, '.', dtype='U1'))
 
-    cubes.appendleft({})
-    cubes.append({})
 
-    return cubes, center_z_index + 1
+def pad_cubes(cubes: CubeType):
+    for i in range(len(cubes)):
+        cubes[i] = np.pad(cubes[i], 1, mode='constant', constant_values='.')
 
 
 class Day17Part1(Day):
@@ -35,17 +36,14 @@ class Day17Part1(Day):
                 "###")
 
     def parse_input(self):
-        return self.input_sample_lines
+        lines = self.input_sample_lines
+        #                                     the len seems to be used to tell the length of each row
+        #                                     <U1 means 'unicode (1 byte?)'
+        return deque([np.array(lines, dtype=('<U1', len(lines[0])))])
 
     def solve(self):
         data = self.parse_input()
-        cubes = expand_cubes(deque(), 0)
-        print(cubes)
-
-
-def test_cubes_expand_on_empty_input():
-    assert len(expand_cubes(deque(), 0)[0]) == 3
-
-
-def test_cubes_expand_on_one_cube_input():
-    assert len(expand_cubes(deque(({},)), 0)[0]) == 3
+        expand_cubes_z(data)
+        pad_cubes(data)
+        pad_cubes(data)
+        print(data[0])
