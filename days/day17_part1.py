e3b531c60da0059782f68c7a082b1270cb69229d
sharkbound <laptopblaster@gmail.com>
2021-09-25 21:51:50 -0500

    reacting the cubes is starting to work, needs to be fixed however

--- days/day17_part1.py
+++ days/day17_part1.py
@@ -1,3 +1,4 @@
+import itertools
 from collections import deque
 from copy import deepcopy
 from typing import Deque, Tuple, Generator
@@ -9,41 +10,68 @@ import numpy as np
 ACTIVE = '#'
 INACTIVE = '.'
 
-CubeType = Deque[np.ndarray]
-
-
-def get_neighbors(cubes: CubeType, x: int, y: int, z: int) -> Generator[str, None, None]:
-    pass
-
-
-def expand_cubes_z(cubes: CubeType):
-    cubes.appendleft(np.full(cubes[0].shape, '.', dtype='U1'))
-    cubes.append(np.full(cubes[0].shape, '.', dtype='U1'))
-
-
-def pad_cubes(cubes: CubeType):
-    for i in range(len(cubes)):
-        cubes[i] = np.pad(cubes[i], 1, mode='constant', constant_values='.')
-
 
 class Day17Part1(Day):
     day = 17
     part = 1
+
+    NEIGHBOR_OFFSETS = tuple(offsets for offsets in itertools.product((-1, 0, 1), repeat=3) if any(offsets))
 
     def get_sample_input(self):
         return (".#.\n"
                 "..#\n"
                 "###")
 
-    def parse_input(self):
-        lines = self.input_sample_lines
-        #                                     the len seems to be used to tell the length of each row
-        #                                     <U1 means 'unicode (1 byte?)'
-        return deque([np.array(lines, dtype=('<U1', len(lines[0])))])
-
     def solve(self):
         data = self.parse_input()
-        expand_cubes_z(data)
-        pad_cubes(data)
-        pad_cubes(data)
-        print(data[0])
+        while True:
+            # create a blank array that is +2 length of the current data, this is to expand its z axis
+            data = self.fill_mid_section(self.create_zero_filled_expanded_z_array(data, 2), data)
+            print(f'===========\nEX: {data}\n=============')
+            blank = np.zeros(data.shape)
+
+            for indexes, value in np.ndenumerate(data):
+                _, alive, dead = self.get_neighbors(data, *indexes)
+                if value == 1:
+                    blank[indexes] = 1 if value in {2, 3} else 0
+                elif value == 0:
+                    blank[indexes] = 1 if alive == 3 else 0
+
+
+            data = self.pad_inner_arrays(blank)
+            print(data)
+            exit()
+
+    def parse_input(self):
+        lines = [[int(symbol == ACTIVE) for symbol in row] for row in self.input_sample_lines]
+        #               the inner array is needed because of the 3D array requirement
+        return np.array([np.array(lines)], dtype=np.bool)
+
+    def get_neighbors(self, data: np.ndarray, z: int, y: int, x: int):
+        # noinspection PyUnboundLocalVariable
+        return (
+            neighbors := tuple(data[actual_z, actual_y, actual_x]
+                               for zoff, yoff, xoff in self.NEIGHBOR_OFFSETS
+                               if 0 <= (actual_z := z + zoff) < data.shape[0]
+                               and 0 <= (actual_y := y + yoff) < data.shape[1]
+                               and 0 <= (actual_x := x + xoff) < data.shape[2]),
+            sum(neighbors),
+            len(neighbors) - sum(neighbors)
+        )
+
+    @staticmethod
+    def create_zero_filled_expanded_z_array(data: np.ndarray, times: int = 2):
+        return np.full(((s := data.shape)[0] + times, s[1], s[2]), fill_value=0)
+
+    @staticmethod
+    def fill_mid_section(to_fill: np.ndarray, fill_data: np.ndarray):
+        to_fill[1:-1] = fill_data
+        return to_fill
+
+    @staticmethod
+    def pad_inner_arrays(data: np.ndarray):
+        shape = data.shape
+        output = np.full((shape[0], shape[1] + 2, shape[2] + 2), fill_value=0)
+        for i, row in enumerate(data):
+            output[i] = np.pad(row, 1, constant_values=0)
+        return output
